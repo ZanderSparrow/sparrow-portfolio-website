44129c83be96b70f638ab83020210cdee3887658
Zander Sparrow <alexsilverhawk@gmail.com>
2023-11-15 15:46:24 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Sparrow Portfolio Website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
